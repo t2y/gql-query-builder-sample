055d29a10b27b87b536ab845fbc6b754f333fbff
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2021-06-07 15:32:36 +0900

    add getSchemaFromQuery to fetch any schema with introspection query

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,11 @@
 import fs from "fs";
-import { buildSchema, GraphQLSchema } from "graphql";
+import {
+  buildClientSchema,
+  buildSchema,
+  getIntrospectionQuery,
+  GraphQLSchema,
+} from "graphql";
+import fetch from "node-fetch";
 
 export function getSchema(
   path: string,
@@ -7,4 +13,34 @@ export function getSchema(
 ): GraphQLSchema {
   const contents = fs.readFileSync(path, { encoding, flag: "r" });
   return buildSchema(contents, { assumeValid: true });
+}
+
+export async function getSchemaFromQuery(
+  endpoint: string = "http://localhost:4000",
+  authorization: string = ""
+): Promise<GraphQLSchema> {
+  const query = getIntrospectionQuery({
+    descriptions: false,
+    schemaDescription: false,
+  });
+  const response = await fetch(`${endpoint}/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: authorization,
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  if (response.status !== 200) {
+    throw new Error(response.statusText);
+  }
+
+  const json = await response.json();
+  if (json.errors !== undefined) {
+    throw new Error(json.errors);
+  }
+  const introspection = json.data;
+  return buildClientSchema(introspection, { assumeValid: true });
 }
